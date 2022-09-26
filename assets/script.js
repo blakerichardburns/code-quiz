97afd2da66b4b7580f89287fa3ca0ff15460aaa5
Blake Burns <bburns@mysgi.com>
2022-09-25 22:51:06 -0400

    added javascript pseudocode skelton

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// declare variables
+
+//click event triggers the following:
+    //quiz-greeting is hidden
+    //timer starts counting down
+    //questions section is written
+    //questions section displays
+
+//click event on correct logs correct
+    //click even on incorrect logs incorrect
+    //2nd version of questions section is written
+
+//click event on correct logs correct
+    //click even on incorrect logs incorrect
+    //3rd version of questions section is written
+
+//click event on correct logs correct
+    //click even on incorrect logs incorrect
+    //4th version of questions section is written
+
+//click event on correct logs correct
+    //click even on incorrect logs incorrect
+    //questions section is hidden
+    //results section appears
+    
+//click event logs initials and score to leaderboard
+    //results section is hidden
+    //leaderboard is displayed
